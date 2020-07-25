1ced113c65965c3f3d5f113a45525dd5427afa70
santosh1817 <santosh01994@gmail.com>
2020-07-25 10:39:48 +0530

    added form

--- client/src/components/RegisterForm/RequestForm.js
+++ client/src/components/RegisterForm/RequestForm.js
@@ -8,9 +8,42 @@ class RequestForm extends React.Component
         this.state={
         createdBy:'',
         department:'',
-        users:'',
+        departmentArray:[],
+        userArray:[],
+        user:'',
         message:''
         }
+    }
+    componentDidMount()
+    {
+      axios.get('http://localhost:3005/category/viewall',{
+          headers:{
+              'x-auth':localStorage.getItem('token')
+          }
+      })
+
+      .then((response)=>{
+          console.log(response,'in category')
+          this.setState(()=>({
+              categories:response.data
+              
+          }))
+    })
+    handleChange=(e)=>{
+        e.persist()
+        this.setState(()=>({
+          [e.target.name] : e.target.value   
+        }))
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault() 
+        const formData={
+            name : this.state.name,
+            department : this.state.department,
+            priority : this.state.priority,
+            message : this.state.message,
+            notice : ''
+    }
     }
     render() {
         return(
@@ -24,8 +57,8 @@ class RequestForm extends React.Component
                     <label className="headerlabel">Username 
                         <input 
                         type="text" 
-                        name="username"
-                        value={this.state.username} 
+                        name="createdBy"
+                        value={this.state.createdBy} 
                         onChange={this.handleChange} 
                         className="form-control" 
                         placeholder="Enter username"
@@ -36,7 +69,7 @@ class RequestForm extends React.Component
                     <div className="form-group col-md-4"></div>
                     <div className="form-group col-md-4"></div>
                     <div className="form-group col-md-2">
-                    <label className="headerlabel">Department 
+                    <label className="headerlabel">Department(to assign) 
                         <select name="department" value={this.state.department} onChange={this.handleChange} className="form-control">
                             <option value="">Select</option>
                             <option value="CS">CS</option>
@@ -46,8 +79,8 @@ class RequestForm extends React.Component
                     </label>
                     </div>
                     <div className="form-group col-md-3">
-                    <label className="headerlabel">Users
-                        <select name="department"   value={this.state.department}   onChange={this.handleChange} className="form-control">
+                    <label className="headerlabel">User (to assign)
+                        <select name="user"   value={this.state.user}   onChange={this.handleChange} className="form-control">
                             <option value="">Select</option>
                             <option value="CS">CS</option>
                             <option value="ME">ME</option>
@@ -58,8 +91,8 @@ class RequestForm extends React.Component
                     <div className="form-group col-md-2"></div>
                     <div className="form-group col-md-4"></div>
                     <div className="form-group col-md-4">
-                    <label for="exampleFormControlTextarea1" className="headerlabel">Message
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" cols="34"></textarea>
+                    <label htmlFor="exampleFormControlTextarea1" className="headerlabel">Message
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" cols="34" value={this.state.message} onChange={this.handleChange} name ="message"></textarea>
                     </label>
                     </div>
                     <div className="form-group col-md-4"></div>
